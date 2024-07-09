5567099b9cd41337f0965e40420b2a409d5d378e
karthikn25 <mkarthikeyan179@gmail.com>
2024-07-09 09:23:33 +0530

    first commit

--- src/Component/User/Forget/Forget.js
+++ src/Component/User/Forget/Forget.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import img from '../../../Images/forget.jpg';
+import React from "react";
+import img from "../../../Images/forget.jpg";
 
 export default function Forget() {
   return (
-    <div className='h-screen bg-blue-400 w-screen flex justify-center items-center overflow-hidden'>
-    <div className='h-5/6 bg-white w-4/5 flex'>
-    <div className='w-3/5 h-full'>
-    <img src={img} alt='forget-img' className='h-full'/>
+    <div className="h-screen bg-blue-400 w-screen flex justify-center items-center overflow-hidden">
+      <div className="h-5/6 bg-white w-4/5 flex">
+        <div className="w-3/5 h-full">
+          <img src={img} alt="forget-img" className="h-full" />
+        </div>
+        <div className="w-2/5 h-full text-center">
+          <h4 className="font-serif font-bold text-2xl ml-30 mt-36">
+            FORGET PASSWORD
+          </h4>
+          <input
+            type="text"
+            className="bg-purple-300 h-10 my-14 pl-8 w-96 placeholder-black placeholder:font-bold rounded-xl"
+            placeholder="Email"
+          />
+          <div className="text-center">
+            <button className="max-w-96 bg-purple-300 h-10 w-96 font-bold text-md rounded-xl font-serif text-blue-700">
+              Reset Password
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className='w-2/5 h-full'>
-    <h4 className='font-serif font-bold text-2xl ml-30 mt-36'>FORGET PASSWORD</h4>
-    <input type='text' className='bg-purple-300 w-96 h-10 my-14'/>
-    </div>
-    </div>
-    </div>
-  )
+  );
 }
