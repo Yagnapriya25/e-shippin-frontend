226a3e468303cbdffa8fc1573b4063d6cd71b51e
karthikn25 <mkarthikeyan179@gmail.com>
2024-09-15 14:25:09 +0530

    first commit

--- src/Redux/actions/userAction.js
+++ src/Redux/actions/userAction.js
@@ -1,5 +1,6 @@
-import { loginFail, loginRequest, loginSuccess } from "../slices/userSlice"
+import { forgetPasswordFail, forgetPasswordRequest, forgetPasswordSuccess, loginFail, loginRequest, loginSuccess, otpFail, otpRequest, otpSuccess, resetPasswordFail, resetPasswordRequest, resetPasswordSuccess, signupFail, signupRequest, signupSuccess } from "../slices/userSlice"
 import { URL } from "../../server";
+
 
 
 
@@ -18,10 +19,105 @@ const login = (credentials)=>async(dispatch)=>{
         const data = await res.json();
         if(res.ok){
             dispatch(loginSuccess(data));
+            localStorage.setItem("id",data.id);
+            localStorage.setItem("token",data.token)
         }else{
             dispatch(loginFail(data.message));
         }
     } catch (error) {
         dispatch(loginFail(error.message));
+    }
+}
+
+const register = (credentials)=>async(dispatch)=>{
+    try {
+        dispatch(signupRequest());
+        const {email,password,username} = credentials;
+        const res = await fetch(`${URL}/user/register`,{
+            method:"POST",
+            body:JSON.stringify(credentials),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        if(res.ok){
+            dispatch(signupSuccess(data)); 
+        } 
+        else{
+            dispatch(signupFail(data.message))
+        }     
+    } catch (error) {
+        dispatch(signupFail(error.message))
+    }
+}
+
+const verifyOtp = (credentials)=>async(dispatch)=>{
+    try {
+        dispatch(otpRequest());
+        const {otp}=credentials;
+        const res = await fetch(`${URL}/verify-otp`,{
+            method:"POST",
+            body:JSON.stringify(credentials),
+            headers:{
+                "Content-Type":"application/json"
+            }
+
+        })
+        const data = await res.json();
+        if(res.ok){
+            dispatch(otpSuccess(data));
+        }
+        else{
+            dispatch(otpFail(data.message));
+        }
+    } catch (error) {
+        dispatch(otpFail(error.message));
+    }
+}
+
+const forget = (credentials)=>async(dispatch)=>{
+    try {
+        dispatch(forgetPasswordRequest());
+        const {email}= credentials;
+        const res = await fetch(`${URL}/user/forget`,{
+            method:"POST",
+            body:JSON.stringify(credentials),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        if(res.ok){
+            dispatch(forgetPasswordSuccess(data));
+        }
+        else{
+            dispatch(forgetPasswordFail(data.message));
+        }
+    } catch (error) {
+        dispatch(forgetPasswordFail(error.message))
+    }
+}
+const reset = (credentials,userData)=>async(dispatch)=>{
+    try {
+        dispatch(resetPasswordRequest());
+        const {password}=credentials;
+        const {id,token}=userData;
+        const res = await fetch(`${URL}/user/reset/${id}/${token}`,{
+            method:"PUT",
+            body:JSON.stringify(credentials),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        if(res.ok){
+            dispatch(resetPasswordSuccess(data));
+        }
+        else{
+            dispatch(resetPasswordFail(data.message));
+        }
+    } catch (error) {
+        dispatch(resetPasswordFail(error.message));
     }
 }
