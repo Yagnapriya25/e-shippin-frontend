7cd2d5c941399004df004e5156feb78f1e9a111e
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-02 21:47:09 +0530

    first commit

--- src/Redux/actions/productAction.js
+++ src/Redux/actions/productAction.js
@@ -1,4 +1,4 @@
-import { productPostRequest } from "../slices/productSlice";
+import { productDeleteFail, productDeleteRequest, productDeleteSuccess, productEditFail, productEditRequest, productEditSuccess, productGetAllFail, productGetAllRequest, productGetAllSuccess, productGetSingleFail, productGetSingleRequest, productGetSingleSuccess, productPostFail, productPostRequest, productPostSuccess } from "../slices/productSlice";
 
 
 
@@ -7,16 +7,129 @@ import { productPostRequest } from "../slices/productSlice";
 
 let URL = "http://localhost:7890/api";
 
-const productPost = (credential)=>(dispatch)=>{
+const productPost = (credential,categoryInfo,userInfo)=>async(dispatch)=>{
     try {
         dispatch(productPostRequest());
-        const {name,description1,description2,description3,price,Instock}=credential;
-        // const res = await fetch(`${URL}/product/create/`)
-
-        
+        const {cat_id}=categoryInfo;
+        const {id}=userInfo;
+        const res = await fetch(`${URL}/product/${cat_id}/${id}`,{
+            method:"POST",
+            body:JSON.stringify(credential),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })        
+        const data = await res.json();
+        console.log(data);
+        if(res.ok){
+            dispatch(productPostSuccess(data))
+        }
+        else{
+            dispatch(productPostFail(data.message))
+        }
         
     } catch (error) {
-        
+        dispatch(productPostFail(error.message))
     }
 
 }
+
+const getAllProduct = ()=>async(dispatch)=>{
+    try {
+        dispatch(productGetAllRequest());
+        const res = await fetch(`${URL}/product/getall`,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        console.log(data);
+        if(res.ok){
+            dispatch(productGetAllSuccess(data))
+        }
+        else{
+            dispatch(productGetAllFail(data.message))
+        }
+    } catch (error) {
+        dispatch(productGetAllFail(error.message))
+    }
+}
+
+const getSingleProduct = (productInfo,userInfo)=>async(dispatch)=>{
+    try {
+        dispatch(productGetSingleRequest());
+        const {id}=productInfo;
+        const {token}=userInfo;
+        const res = await fetch(`${URL}/product/getsingle/${id}`,{
+            method:"GET",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        console.log(data);
+        if(res.ok){
+            dispatch(productGetSingleSuccess(data))
+        }
+        else{
+            dispatch(productGetSingleFail(data.message))
+        }
+    } catch (error) {
+        dispatch(productGetSingleFail(error.message))
+    }
+}
+const deleteProduct = (productInfo)=>async(dispatch)=>{
+    try {
+        dispatch(productDeleteRequest());
+        const {id} = productInfo;
+        const res = await fetch(`${URL}/product/remove/${id}`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        console.log(data);
+        if(res.ok){
+            dispatch(productDeleteSuccess(data))
+        }
+        else{
+            dispatch(productDeleteFail(data.message))
+        }
+    } catch (error) {
+        dispatch(productDeleteFail(error.message))
+
+    }
+}
+const updateProduct = (credential,productInfo)=>async(dispatch)=>{
+    try {
+        dispatch(productEditRequest());
+        const {id} = productInfo;
+        const res = await fetch(`${URL}/product/edit/${id}`,{
+            method:"PUT",
+            body:JSON.stringify(credential),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        const data = await res.json();
+        console.log(data);
+        if(res.ok){
+            dispatch(productEditSuccess(data))
+        }
+        else{
+            dispatch(productEditFail(data.message))
+        }
+    } catch (error) {
+         dispatch(productDeleteFail(error.message))   
+    }
+}
+
+export {
+    productPost,
+    getAllProduct,
+    getSingleProduct,
+    deleteProduct,
+    updateProduct
+};
--- src/Redux/slices/productSlice.js
+++ src/Redux/slices/productSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  productInfo: null,
+  productInfo: [],
   loading: true,
   error: null,
 };
