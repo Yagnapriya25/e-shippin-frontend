381031379186b8c74d328062d985948f2a4736a5
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-10 09:47:51 +0530

    first commit

---
+++ src/Component/Loading.js
@@ -0,0 +1,18 @@
+// Loading.js
+import React from 'react';
+import Base from '../Base/Base';
+
+const Loading = () => {
+  return (
+    <Base>  <div className="flex flex-col items-center justify-center h-screen">
+      <div className="loader mb-2">
+        <div className="w-16 h-16 border-8 border-t-8 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+      <p className="text-lg text-gray-700">Loading...</p>
+    </div>
+    </Base>
+  
+  );
+};
+
+export default Loading;
