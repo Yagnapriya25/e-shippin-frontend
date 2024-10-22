75347593f33844cdfa353c32b734ae7ba1d318cd
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-22 21:34:07 +0530

    first commit

--- src/Component/UserProducts.js
+++ src/Component/UserProducts.js
@@ -72,7 +72,7 @@ export default function UserProducts() {
                                             <h6 className="capitalize xl:text-xl text-center lg:text-lg md:text-md text-sm font-serif">
                                                 {product.name}
                                             </h6>
-                                            <h5 className="font-bold px-5 xl:text-xl lg:text-lg md:text-md text-sm">
+                                            <h5 className="font-bold px-5 xl:text-xl lg:text-lg text-center md:text-md text-sm">
                                                 {formatPrice(product.price)}
                                             </h5>
                                         </div>
