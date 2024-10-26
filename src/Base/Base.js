f95979ba0e3eb96201f89839927fee7b60e93cd3
karthikn25 <mkarthikeyan179@gmail.com>
2024-10-26 12:41:38 +0530

    first commit

--- src/Base/Base.js
+++ src/Base/Base.js
@@ -7,27 +7,27 @@ export default function Base({ children }) {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen w-screen flex flex-col overflow-hidden hide-scrollbar">
-      <div className="sticky top-2.5 mx-5 my-0 z-10">
+    <div className="h-screen w-screen overflow-hidden hide-scrollbar">
+      <div className="h-28 md:h-28 lg:h-20 xl:h-20 sticky top-2.5 mx-5 my-0">
         <Topbar />
       </div>
-      <div className="flex-grow mx-1 lg:mx-4 xl:mx-5 md:mx-3 mt-5 mb-10 md:mb-0 overflow-y-auto hide-scrollbar">
+      <div className="mx-1 my-10 xl:my-5 lg:mx-4 xl:mx-5 md:mx-3 overflow-hidden overflow-x-hidden hide-scrollbar mb-80 xl:mb-10 lg:mb-10">
         {children}
       </div>
-      <div className="block sticky bottom-0 md:hidden">
+      <div className="block sticky bottom-0 md:hidden ">
         <div className="bg-indigo-300">
-          <ul className="flex justify-around text-white">
+          <ul className="flex justify-around text-white ">
             <li>
-              <i className="bx bx-home-alt hover:text-red-400 cursor-pointer" onClick={() => navigate(`/home/${token}`)}></i>
+              <i class="bx bx-home-alt hover:text-red-400 cursor-pointer" onClick={()=>navigate(`/home/${token}`)}></i>
             </li>
             <li>
-              <i className="bx bx-category hover:text-red-400 cursor-pointer" onClick={() => navigate(`/category/${token}`)}></i>
+              <i class="bx bx-category hover:text-red-400 cursor-pointer" onClick={()=>navigate(`/category/${token}`)}></i>
             </li>
             <li>
-              <i className="bx bx-cart hover:text-red-400 cursor-pointer" onClick={() => navigate(`/cart/${token}`)}></i>
+              <i class="bx bx-cart hover:text-red-400 cursor-pointer" onClick={()=>navigate(`/cart/${token}`)}></i>
             </li>
             <li>
-              <i className="bx bx-user hover:text-red-400 cursor-pointer" onClick={() => navigate(`/profile/${token}`)}></i>
+              <i class="bx bx-user hover:text-red-400 cursor-pointer"  onClick={()=>navigate(`/profile/${token}`)}></i>
             </li>
           </ul>
         </div>
--- src/Base/Topbar.js
+++ src/Base/Topbar.js
@@ -36,10 +36,10 @@ export default function Topbar() {
   return (
     <div className="h-2/6 md:h-4/6 pt-0 lg:pt-0 xl:pt-0 shadow-gray-700 sticky top-2.5 bg-white shadow ">
       <div className="flex justify-around">
-        <div className="h-8 my-2 lg:h-12 lg:mx-4 lg:my-3 xl:h-14 xl:mx-6 xl:my-1 md:h-10 md:mx-4 md:my-3">
+        <div className="h-8 my-2 lg:h-12 lg:mx-4 lg:my-3 xl:h-14 xl:mx-6 xl:my-3 md:h-10 md:mx-4 md:my-3">
           <img src={logo} alt="logo" className="h-full w-16" />
         </div>
-        <span className="my-3 flex gap-2 lg:my-5 lg:flex lg:gap-3 xl:my-5 xl:flex xl:gap-3 md:my-5 md:flex md:gap-3">
+        <span className="my-3 flex gap-2 lg:my-5 lg:flex lg:gap-3 xl:my-8 xl:flex xl:gap-3 md:my-5 md:flex md:gap-3">
           <input
             type="text"
             className="pl-5 w-40 h-6 md:w-56 lg:h-6 lg:w-30 lg:pl-10 border-2 outline-none xl:h-7 xl:w-30 xl:pl-10 md:h-7 md:w-18 md:pl-10 "
@@ -49,7 +49,7 @@ export default function Topbar() {
             
           />
           <button onClick={handleSearch}>
-          <i className="bx bx-search text-red-400 pt-0 xl:absolute xl:top-6 lg:absolute lg:top-6 lg:text-xl xl:text-xl md:text-lg cursor-pointer"></i>
+          <i className="bx bx-search text-red-400 pt-1 lg:text-xl xl:text-xl md:text-lg cursor-pointer"></i>
           </button>
         </span>
         <ul className="hidden lg:flex lg:gap-10 lg:my-5 md:block lg:block xl:block xl:flex md:flex xl:gap-10 md:gap-10 xl:my-8 md:my-5">
